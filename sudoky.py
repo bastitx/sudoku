d3042ce64e0937d2b57e1230b0f98d528b016022
Bastian Harren <bastian@harren.co>
2020-02-13 16:12:34 +0100

    init

---
+++ sudoky.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class SudokuSolver():
+    def __init__(self, grid):
+        self.grid = grid
+
+    def possible(self, x, y, n):
+        return True
+    
+    def solve(self):
+        return
